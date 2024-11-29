ca2eb9461f058e31b7c55b9bb69e41d6e556ff04
dukehootfish <fishelad01@gmail.com>
2024-11-29 16:39:50 +0200

    -

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -5,8 +5,18 @@ public class ArmConstants {
     public static final int wristPWMchannel = 2;
 
     public static final double kp = 0;
+    public static final double kfBase = 0;
+
+    public static final double squareVelocity = 0;
+    public static final double circleVelocity = 0;
+    public static final double offVelocity = 0;
+
+    public static final double armlength1 = 300;
+    public static final double armlength2 = 200;
 
     private static final double gearRatio = 1.0 / 192.0;
     public static final double positionFactor = 360 * gearRatio; // deg/rotations
     public static final double velocityFactor = 360 * gearRatio / 60; // (deg/sec)/RPM
+
+
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems;
 
+
 import frc.robot.motors.PIDFGains;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Servo;
 import frc.robot.constants.ArmConstants;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ArmConstants;
 import frc.robot.motors.DBugSparkMax;
@@ -11,12 +15,123 @@ import frc.robot.motors.DBugSparkMax;
 public class Arm extends SubsystemBase{
 
     private DBugSparkMax _base;
-    private Servo  _wrist;
+    private Servo _wrist;
+    private double servoZeroPos;
+
+    private BaseState baseState;
+
+    public static enum BaseState {
+        CIRCLE(ArmConstants.circleVelocity),
+        SQUARE(ArmConstants.squareVelocity),
+        OFF(ArmConstants.offVelocity);
+
+        public final double velocity;
+
+        private BaseState(double velocity){
+            this.velocity = velocity;
+        }
+    }
 
     public Arm(){
-        _base = DBugSparkMax.create(ArmConstants.baseCANID,  new PIDFGains(ArmConstants.kp),
+        _base = DBugSparkMax.create(ArmConstants.baseCANID,  new PIDFGains(ArmConstants.kp, 0, 0, ArmConstants.kfBase),
                 ArmConstants.positionFactor, ArmConstants.velocityFactor, 0);
         _wrist = new Servo(ArmConstants.wristPWMchannel);
+
+        servoZeroPos = _wrist.getAngle();
+
     }
+
+    public void resetPosotion(){
+        this.servoZeroPos = _wrist.getAngle();
+    }
+
+    public void setWristAngle(double angle){
+        _wrist.setAngle(angle + servoZeroPos);
+    }
+
+    public double getWristAngle(){
+        return this._wrist.getAngle() - servoZeroPos;
+    }
+
+    public void setBaseState(BaseState state){
+        this.baseState = state;
+    }
+
+    public double getBaseAngle(){
+        return this._base.getPosition();
+    }
+
+    public void drawCircle(double R){
+
+        double wristAngle = Math.acos((Math.pow(ArmConstants.armlength1, 2)+ Math.pow(ArmConstants.armlength2, 2) - Math.pow(R, 2))/ 2 * ArmConstants.armlength1 * ArmConstants.armlength2);
+        this.setWristAngle(wristAngle);
+        double startBaseAngle = getBaseAngle();
+        this._base.set(BaseState.CIRCLE.velocity);
+
+        while (startBaseAngle != getBaseAngle()) {
+            this.setWristAngle(wristAngle);
+        }
+
+        this._base.set(BaseState.OFF.velocity);
+    }
+
+    public void drawSquare(){
+        double radius = Math.sqrt((Math.pow(ArmConstants.armlength1, 2)+ Math.pow(ArmConstants.armlength2, 2) - 2 * ArmConstants.armlength1 * ArmConstants.armlength2) * Math.cos(getWristAngle()));
+        double startBaseAngle = getBaseAngle()%360;
+        this._base.set(BaseState.SQUARE.velocity);
+        double ARM_LENGTH3;
+        double targetY;
+        double targetX;
+        while (startBaseAngle != getBaseAngle()) {
+            double angle1 = (getBaseAngle()-startBaseAngle) % 360;
+
+            if(320 < angle1 || angle1 <= 45 ) {
+                targetX = radius;
+
+                double x1 = ArmConstants.armlength1 * Math.cos(angle1);
+                double x2 = targetX-x1;
+                double y1 = Math.sqrt((ArmConstants.armlength2*ArmConstants.armlength2)-(x2*x2));
+                double y2 = ArmConstants.armlength1 * Math.sin(angle1);
+                targetY = y2+y1;
+            }
+            else if(angle1 <= 135) {
+                targetY = radius;
+
+                double y1 = ArmConstants.armlength1 * Math.sin(angle1);
+                double y2 = targetY-y1;
+                double x1 = Math.sqrt((ArmConstants.armlength2*ArmConstants.armlength2)-(y2*y2));
+                double x2 = ArmConstants.armlength1 * Math.cos(angle1);
+                targetX =  x2-x1;
+            }
+            else if((angle1 <= 225)){
+
+                targetX = -radius;
+
+                double x1 = ArmConstants.armlength1 * Math.cos(angle1);
+                double x2 = targetX-x1;
+                double y1 = Math.sqrt((ArmConstants.armlength2*ArmConstants.armlength2)-(x2*x2));
+                double y2 = ArmConstants.armlength1 * Math.sin(angle1);
+                targetY = y2-y1;
+            }
+            else {
+                targetY = -radius;
+
+                double y1 = ArmConstants.armlength1 * Math.sin(angle1);
+                double y2 = targetY-y1;
+                double x1 = Math.sqrt((ArmConstants.armlength2*ArmConstants.armlength2)-(y2*y2));
+                double x2 = ArmConstants.armlength1 * Math.cos(angle1);
+                targetX =  x2+x1;
+            }
+
+            ARM_LENGTH3=Math.sqrt(targetX*targetX+targetY*targetY);
+
+            double wristAngle = Math.acos((Math.pow(ArmConstants.armlength1, 2)+ Math.pow(ArmConstants.armlength2, 2) - Math.pow(ARM_LENGTH3, 2))/ 2 * ArmConstants.armlength1 * ArmConstants.armlength2);
+            setWristAngle(wristAngle);
+        }
+        this._base.set(BaseState.OFF.velocity);
+
+    }
+
+
 
 }
